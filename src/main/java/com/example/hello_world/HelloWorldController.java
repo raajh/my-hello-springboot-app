2530c5c08cf7f626ae764dab6e092de1c7c69c22
Raajh <raajhganshekar@gmail.com>
2024-07-08 16:47:37 +0530

    Update cloudbuild.yaml and HelloWorldController.java

--- src/main/java/com/example/hello_world/HelloWorldController.java
+++ src/main/java/com/example/hello_world/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
     @GetMapping("/")
     public String helloWorld() {
-        return "Hello World! how are you raaj and everything is fine and wine and dine....kkhjsssfdfdfhksdsd";
+        return "Hello World! how are you raaj and everything is fine and wine and dine....kkhjsssfdfdfhksdsssd";
     }
 }
