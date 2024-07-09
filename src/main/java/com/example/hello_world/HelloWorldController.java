f3b4fd3760aeda065a5cd489baf4908c723fe99f
Raajh <raajhganshekar@gmail.com>
2024-07-09 12:32:16 +0530

    Update HelloWorldController

--- src/main/java/com/example/hello_world/HelloWorldController.java
+++ src/main/java/com/example/hello_world/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
     @GetMapping("/")
     public String helloWorld() {
-        return "Hello World! how are you raaj and everything is fine and rain dsd   aaad wine and raiddn";
+        return "Hello World! how are you raaj and everything is fine and rain dsd  ew aaad wine and raiddn";
     }
 }
