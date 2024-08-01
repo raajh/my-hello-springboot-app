d68c90adb2879146c27da8e4116cdcb46a0b1740
ghraj-digi <raajhganshekar@gmail.com>
2024-08-02 01:15:27 +0530

    Describe the changes made to HelloWorldController.java

--- src/main/java/com/example/hello_world/HelloWorldController.java
+++ src/main/java/com/example/hello_world/HelloWorldController.java
@@ -9,7 +9,7 @@ public class HelloWorldController {
     @GetMapping("/")
 
     public String helloWorld() {
-        return "Hello World! today is monday...and ganni and rani..s.ssslllhkkhhls";
+        return "Hello World! today is monday...";
     }
       @GetMapping("/health")
     public String healthCheck() {
