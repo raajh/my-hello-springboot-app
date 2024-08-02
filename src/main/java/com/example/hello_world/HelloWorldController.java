e566906bc61cdc65ecaeb1ab6113b3f362d1865e
ghraj-digi <raajhganshekar@gmail.com>
2024-08-02 14:52:42 +0530

    jenkins

--- src/main/java/com/example/hello_world/HelloWorldController.java
+++ src/main/java/com/example/hello_world/HelloWorldController.java
@@ -9,7 +9,7 @@ public class HelloWorldController {
     @GetMapping("/")
 
     public String helloWorld() {
-        return "Hello World! today is monday......zaafsxxssf";
+        return "Hello World! today is monday....";
     }
       @GetMapping("/health")
     public String healthCheck() {
