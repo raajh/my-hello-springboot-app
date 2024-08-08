8e4a0964c5f4c0f1622691d4dd2632eb60d81d78
ghraj-digi <raajhganshekar@gmail.com>
2024-08-08 16:10:42 +0530

    Describe controller

--- src/main/java/com/example/hello_world/HelloWorldController.java
+++ src/main/java/com/example/hello_world/HelloWorldController.java
@@ -9,7 +9,7 @@ public class HelloWorldController {
     @GetMapping("/")
 
     public String helloWorld() {
-        return "Hello World! today is monday......";
+        return "Hello World! today is monday.....ss.";
     }
       @GetMapping("/health")
     public String healthCheck() {
